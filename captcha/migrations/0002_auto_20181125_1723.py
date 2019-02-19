18d2b4d2a469df276276d9936cace2edcb27c74e
Younger Shen <younger.x.shen@gmail.com>
2019-02-19 18:10:23 +0800

    fix: change migrations file name

--- captcha/migrations/0002_auto_20181125_1723.py
+++ captcha/migrations/0002_auto_20181125_1723.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('decaptcha', '0001_initial'),
+        ('captcha', '0001_initial'),
     ]
 
     operations = [

b4808f48f57758477597bdb89600d17e57ab9b4d
Younger Shen <younger.x.shen@gmail.com>
2019-02-22 11:02:33 +0800

    test: remove pytest for test ,now just use django test

--- tests/__init__.py
+++
@@ -1,7 +0,0 @@
-# PROJECT : django-easy-captcha
-# TIME : 19-2-21 下午4:03
-# AUTHOR : Younger Shen
-# EMAIL : younger.x.shen@gmail.com
-# CELL : 13811754531
-# WECHAT : 13811754531
-# WEB : https://youngershen.com
